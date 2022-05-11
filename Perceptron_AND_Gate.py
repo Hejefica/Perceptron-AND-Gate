e4950c5a1c31efde05f7ab1732ad494e7cd56faa
Héctor Figueroa <69779567+Hejefica@users.noreply.github.com>
2022-05-11 13:35:43 -0500

    Final Commit.

--- Perceptron_AND_Gate.py
+++ Perceptron_AND_Gate.py
@@ -7,24 +7,25 @@ DataSet = np.array([[1, -1, -1, -1],
                     [1,  1,  1,  1]])
 
 GateOutput = np.array([DataSet[0, 3], DataSet[1, 3], DataSet[2, 3], DataSet[3, 3]])
-W = np.array([1, 1, 0.5])
+IterationComparison = np.array([1, 1, 0.5])
 
 Iteration = 1
-for cont in range(12):  
-    if (cont % 4) == 0:
-        cont = 0    
-    if ((cont -1) % 4) == 0:
-        cont = 1 
-    if ((cont -2) % 4) == 0:
-        cont = 2
-    if ((cont -3) % 4) == 0:
-        cont = 3  
+for Cont in range(12):  
+    if (Cont % 4) == 0:
+        Cont = 0    
+    if ((Cont -1) % 4) == 0:
+        Cont = 1 
+    if ((Cont -2) % 4) == 0:
+        Cont = 2
+    if ((Cont -3) % 4) == 0:
+        Cont = 3  
 
-    PerceptronInput = np.array([DataSet[cont, 0], DataSet[cont, 1], DataSet[cont, 2]])
-    PerceptronOutput = np.sign(np.dot(np.transpose(W), PerceptronInput))
-    W = W + (Alpha*(GateOutput[cont] - PerceptronOutput) * PerceptronInput)
+    PerceptronInput = np.array([DataSet[Cont, 0], DataSet[Cont, 1], DataSet[Cont, 2]])
+    PerceptronOutput = np.sign(np.dot(np.transpose(IterationComparison), PerceptronInput))
+    IterationComparison = IterationComparison + (Alpha*(GateOutput[Cont] - PerceptronOutput) * PerceptronInput)
+    
     print("Iteration #", Iteration)
-    Iteration += 1
     print("     Perceptron Input:", PerceptronInput)
-    print("     Iterative Comparison:", W)
+    print("     Iteration Comparison:", IterationComparison)
     print("     Perceptron Output:", int(PerceptronOutput))
+    Iteration += 1
